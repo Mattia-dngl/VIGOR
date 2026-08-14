7de1f7d5a90aab6b0b97579f9b8b638fc62b9f87
Mattia-dngl <136310582+Mattia-dngl@users.noreply.github.com>
2026-08-14 08:45:24 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 // Quando pubblichi una modifica, alza il numero di VERSIONE qui sotto:
 // l'app se ne accorge da sola e propone di aggiornarsi.
 // ============================================================
-const VERSIONE = "fitpro-v27";
+const VERSIONE = "fitpro-v28";
 
 const DA_TENERE = [
   "./",
