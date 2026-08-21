40bdec66ed456c015714adeeb881a7297aff8b44
Mattia-dngl <136310582+Mattia-dngl@users.noreply.github.com>
2026-08-21 18:49:07 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 // Quando pubblichi una modifica, alza il numero di VERSIONE qui sotto:
 // l'app se ne accorge da sola e propone di aggiornarsi.
 // ============================================================
-const VERSIONE = "vigor-v70";
+const VERSIONE = "vigor-v71";
 
 const DA_TENERE = [
   "./",
